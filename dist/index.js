2f0aa7f4bf2a76aaacfc5ae3a94c75c90e313dd3
tahmina512 <tahmina_tisha@naiyani.com>
2023-08-01 23:14:23 +0600

    typeScript_Basics

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+;
+let mySize = 3;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+// using annotation
+// let age: number=20;
+// if(age<50)
+//     age+=10;
+//     console.log(age);
+
+//     function check(document:any)
+//     {
+//         console.log(document);
+//     }
+
+//use PascalCase
+const enum Size {Small=1,Medium,Large};
+let mySize: Size=Size.Large;
+console.log(mySize);
